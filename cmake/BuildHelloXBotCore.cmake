2d5d4df7a0ee0710216b743d51a1e783c78644c5
Martijn Zeestraten <martijnzeestraten@gmail.com>
2017-12-13 15:33:05 +0100

    Added cmake file

---
+++ cmake/BuildHelloXBotCore.cmake
@@ -0,0 +1,12 @@
+# HelloXBotCore
+include(YCMEPHelper)
+include(FindOrBuildPackage)
+
+
+# files.
+ycm_ep_helper(  HelloXBotCore
+                TYPE GIT
+                STYLE GITHUB
+                REPOSITORY MartijnZ/HelloXBotCore.git
+                COMPONENT external
+            )
